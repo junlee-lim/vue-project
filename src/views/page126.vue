ed55542899896f8458d3689cbc028d2aa81f3f9d
junlee <myyouthhada@naver.com>
2025-12-30 17:23:15 +0900

    오후12.30

--- src/views/page126.vue
+++ src/views/page126.vue
@@ -2,19 +2,22 @@
 import axios from 'axios';
 import { onMounted, reactive } from 'vue';
 
-onMounted(() => {
-    console.log('onMounted');
-    axios.get('https://picsum.photos/v2/list')
-            .then(res => console.log(res.data))
-})
-
 const state = reactive({
     imgList: []
 })
+
+onMounted(() => {
+    console.log('onMounted');
+    axios.get('https://picsum.photos/v2/list')
+            .then(res => state.imgList = res.data)
+})
+
 </script>
 
 <template>
 <h3>page126</h3>
+<img v-for="item in state.imgList" :key="idx" :src="item.download_url">
+
 </template>
 
 <style scoped>
