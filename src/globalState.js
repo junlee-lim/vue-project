57a4036d23e8c3400eee81eeb5d34ec5e7010142
junlee <myyouthhada@naver.com>
2026-01-13 10:38:23 +0900

    1.13

--- src/globalState.js
+++ src/globalState.js
@@ -1,4 +1,4 @@
 export const globalState = [
     "components", 'page98', 'page103', 'page108', 'page110', 'page114', 'page118', 'page123', 'page125',
-    'page126', 'page129', 'm_1'
+    'page126', 'page129', 'm_1', 'airbnb'
 ]
